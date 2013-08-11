862e13a0d1e3782f3a2929de012afafeeb78042d
Rasmus Bååth <rasmus.baath@gmail.com>
2013-08-11 18:56:03 +0200

    Implemented a jags model for the correlation test using a multivariate t distribution.

--- R/bfa_cor_test.R
+++ R/bfa_cor_test.R
@@ -2,26 +2,34 @@ jags_cor_test <- function(x1, x2) {
   model_string <- "
     model {
       for(i in 1:n) {
-        x[i,1:2] ~ dmnorm(mu[], prec[ , ])
-        #x[i,1:2] ~ dmt(mu[], prec[ , ], nu)  # <- not working
+        #x[i,1:2] ~ dmnorm(mu[], prec[ , ])
+        x[i,1:2] ~ dmt(mu[], prec[ , ], nu) 
       }
 
       ## priors for elements of precision matrix
-      prec[1:2,1:2] ~ dwish(R[,],k)
-      R[1,1] <- .01
-      R[1,2] <- 0
-      R[2,1] <- 0
-      R[2,2] <- .01
-      k <- 2
+      prec[1:2,1:2] <- inverse(cov[,])
+      
+      cov[1,1] <- sigma[1] * sigma[1]
+      cov[1,2] <- sigma[1] * sigma[2] * rho
+      cov[2,1] <- sigma[1] * sigma[2] * rho
+      cov[2,2] <- sigma[2] * sigma[2]
+      
+      sigma[1] ~ dunif(0, 1000) 
+      sigma[2] ~ dunif(0, 1000)
+      tau[1] <- 1 / pow(sigma[1], 2)
+      tau[2] <- 1 / pow(sigma[2], 2)
+      rho ~ dunif(-1, 1)
+
       mu[1] ~ dnorm(0, 0.0001)
       mu[2] ~ dnorm(0, 0.0001)
-      sigma[1:2,1:2] <- inverse(prec[ , ])
-      rho <- sigma[1,2]/sqrt(sigma[1,1]*sigma[2,2])
-      nu ~ dunif(2, 250)
+
+      nu <- nuMinusOne+1
+      nuMinusOne ~ dexp(1/29)
     }
   "
   
-  jags_model <- jags.model(textConnection(model_string), data=list(x = cbind(x1, x2), n = length(x1)), n.adapt= 1000)
+  jags_model <- jags.model(textConnection(model_string), data=list(x = cbind(x1, x2), n = length(x1)), n.adapt= 1000, 
+                           inits = list(mu=c(median(x1), median(x2)), rho=cor(x1, x2, method="spearman"), sigma = c(mad(x1), mad(x2))))
   update(jags_model, 1000)
   mcmc_samples <- coda.samples(jags_model, c("mu", "rho", "sigma", "nu"), n.iter=1000)
   mcmc_samples
--- notes.md
+++ notes.md
@@ -3,16 +3,19 @@ Bayesian First Aid
 
 Tests to include:
 
-  * t.test
+  * t.test 
+    - Using BEST
   * cor.test
+    - Maybe fit multivariate t-dist. Or something non-parametric?
   * binom.test
+    - Just fit regular Bernouli distribution
   * chisq.test & fisher.test
+    - Pointers to how to do this are here: http://andrewgelman.com/2009/10/13/what_is_the_bay/ and http://lingpipe-blog.com/2009/10/13/bayesian-counterpart-to-fisher-exact-test-on-contingency-tables/. These are just for the 2x2 case and I'm not sure how to generalize it. chisq.test can be used for many  things. Maybe start by implementing it just for the common 2x2 case.
   * aov
   * lm
   * var.test
+    - Same as t.test but change the output to reflect the comparison between the variances.
   * poisson.test
+    - Robustify the poisson test by using a negative-binomial distribution but construct it by pasting together a Poisson with a gamma distributed mean as on p. 253 in the Bugs book.
   
-```{r}
 
-
-```
