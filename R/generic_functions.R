20fa6e3ae1db0adcdb9fde1fe65a8a9caf78f881
Rasmus Bååth <rasmus.baath@gmail.com>
2014-01-31 22:25:06 +0100

    Added as.matrix and as.data.frams functions to a Bayesian first aid objects.

--- R/generic_functions.R
+++ R/generic_functions.R
@@ -15,10 +15,6 @@ diagnostics <- function(fit) {
   UseMethod("diagnostics")
 }
 
-
-
-#' 
-
 #' Prints code that replicates the model you just ran. 
 #' 
 #' This is good if you better want to understand how the model is 
@@ -33,4 +29,34 @@ diagnostics <- function(fit) {
 #' @export
 model.code <- function(fit) {
   UseMethod("model.code")
+}
+
+#' Returns the MCMC samples from a Bayesian First Aid fit as a data frame.
+#' 
+#' Returns a dataframe with the MCMC samples from a Bayesian First Aid Object. The samples does not include the burn in phase and all chains are collapsed in the resulting data frame.
+#' 
+#' Details details details
+#' 
+#' @param fit
+#' 
+#' @return
+#' A data frame
+#' @export
+as.data.frame.bayesian_first_aid <- function(x, ...) {
+  as.data.frame(as.matrix(fit$mcmc_samples))
+}
+
+#' Returns the MCMC samples from a Bayesian First Aid fit as a matrix.
+#' 
+#' Returns a matrix with the MCMC samples from a Bayesian First Aid Object. The samples does not include the burn in phase and all chains are collapsed in the resulting matrix.
+#' 
+#' Details details details
+#' 
+#' @param fit
+#' 
+#' @return
+#' A matrix
+#' @export
+as.matrix.bayesian_first_aid <- function(x, ...) {
+  as.matrix(fit$mcmc_samples)
 }
