0c325ba03953cd1a6f651729ce4a202273c27f8f
Rasmus Bååth <rasmus.baath@gmail.com>
2014-03-14 10:06:21 +0100

    Added documentation to the as.data.frame and as.matrix functions.

--- R/generic_functions.R
+++ R/generic_functions.R
@@ -33,29 +33,51 @@ model.code <- function(fit) {
 
 #' Returns the MCMC samples from a Bayesian First Aid fit as a data frame.
 #' 
-#' Returns a dataframe with the MCMC samples from a Bayesian First Aid Object. The samples does not include the burn in phase and all chains are collapsed in the resulting data frame.
+#' Returns a dataframe with the MCMC samples from a Bayesian First Aid Object. 
+#' The samples does not include the burn in phase and all chains are collapsed 
+#' in the resulting data frame.
 #' 
-#' Details details details
+#' In the case where there are many parameters with the same name but different 
+#' index (for example, mu[1], mu[2], mu[3], etc.) these names will be rewritten 
+#' by removing the brackets (that is, mu[1] -> mu1, mu[2] -> mu2, sigma[1,2] ->
+#' sigma12, etc.).
 #' 
-#' @param fit
+#' @param x The output from a Bayesian First Aid model.
+#'   
+#' @return A data frame with one column per parameter.
 #' 
-#' @return
-#' A data frame
+#' @examples
+#' 
+#' fit <- bayes.t.test(rnorm(10), rnorm(10))
+#' d <- as.data.frame(fit)
+#' mean(d$mu_x)
 #' @export
 as.data.frame.bayesian_first_aid <- function(x, ...) {
-  as.data.frame(as.matrix(x$mcmc_samples))
+  d <- as.data.frame(as.matrix(x$mcmc_samples))
+  colnames(d) <- str_replace_all(colnames(d), "\\[|\\]", "")
+  d
 }
 
 #' Returns the MCMC samples from a Bayesian First Aid fit as a matrix.
 #' 
-#' Returns a matrix with the MCMC samples from a Bayesian First Aid Object. The samples does not include the burn in phase and all chains are collapsed in the resulting matrix.
+#' Returns a matrix with the MCMC samples from a Bayesian First Aid Object. The
+#' samples does not include the burn in phase and all chains are collapsed in
+#' the resulting matrix.
 #' 
-#' Details details details
+#' This is just a wrapper for the \code{as.matrix} from the coda package.
+#' Therefor, paramameters with the same name but different index will be column
+#' names that includes brackets (for example, "mu[1]", "mu[2]", "sigma[1,2],
+#' etc.)
 #' 
-#' @param fit
+#' @param x The output from a Bayesian First Aid model.
+#'   
+#' @return A matrix with one column per parameter.
 #' 
-#' @return
-#' A matrix
+#' @examples
+#' 
+#' fit <- bayes.t.test(rnorm(10), rnorm(10))
+#' d <- as.matrix(fit)
+#' mean(d[,"mu_x"])
 #' @export
 as.matrix.bayesian_first_aid <- function(x, ...) {
   as.matrix(x$mcmc_samples)
